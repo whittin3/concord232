9176fb8632fc46c7322af7a46fc1a7a931389581
Neal Whittington <{ID}+{username}@users.noreply.github.com>
2023-11-26 22:57:09 -0600

    adding logging

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ DEVICE_ARG="$(bashio::config 'device')"
 DEBUG_ARG="$(bashio::config 'debug')"
 declare -a OPTIONAL_ARG_ARRAY=()
 if $DEBUG_ARG; then
-  OPTIONAL_ARG_ARRAY+=('--debug');
+  OPTIONAL_ARG_ARRAY+=('--debug true');
 fi
 OPTIONAL_ARGS=$(IFS=" "; echo "${OPTIONAL_ARG_ARRAY[*]}")
 concord232_server --serial "$DEVICE_ARG" "$OPTIONAL_ARGS"
